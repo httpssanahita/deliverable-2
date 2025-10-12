e1e9484e09ac79accc6325ecb7e5f2c33461f417
httpssanahita <ananiavarani@gmail.com>
2025-10-12 18:45:08 -0400

    data uploading
    uploaded the data using the pandas module

--- main_script.py
+++ main_script.py
@@ -1,11 +1,11 @@
 # Names: Jasmine Duong Brisebois, Anahita Niavarani, Ruby Freedman
 # Deliverable-2: a project to observe the relation between the air quality of multiple cities around the world and respiratory health outcomes.
 
-import numpy as np
+import pandas as pd
 
 # Loading the dataset
 
-data= np.loadtxt ("air_quality_health_dataset.csv", skiprows=1 , usecols=(1,2,3,6,7,9))
+data= pd.read_csv (r'C:\Users\anani\OneDrive\Documents\GitHub\deliverable-2\air_quality_health_dataset.csv')
 
 # Attributing numpy arrays to each coloun
 
@@ -17,4 +17,3 @@ col5= data[:,6]
 col6= data[:,7]
 col7= data[:,9]
 
-print("Coloumn 1:", col3)
